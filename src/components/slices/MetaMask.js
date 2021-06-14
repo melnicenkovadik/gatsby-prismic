867afab25e8b477dfc037cf59d7ef043c6cc02b2
melnicenkovadik <melnicenkovadik@gmail.com>
2021-06-14 23:25:45 +0300

    commit2222

--- src/components/slices/MetaMask.js
+++ src/components/slices/MetaMask.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import RichText from "prismic-reactjs/src/Component";
 import useAuth from "./../../hooks/useAuth";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+import {MdContentCopy} from "react-icons/md";
+import {GrStatusGood} from "react-icons/all";
 
 const MetaMask = ({slice}) => {
+    const [text, setText] = useState("");
+    const [isCopied, setIsCopied] = useState(false);
     const {login, logout, currentUser} = useAuth();
     const user = currentUser();
     const userAddress = user?.get("ethAddress");
 
-    React.useEffect(() => {
-    }, [user]);
+    const onCopyText = () => {
+        setText(userAddress)
+        setIsCopied(true);
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 1000);
+    };
+
     return (
         <section className="meta-mask__container">
             <RichText render={slice.primary.meta_title.raw}/>
             <div className="meta-mask">
-
-
                 {
                     !user ?
                         <>
@@ -32,7 +41,19 @@ const MetaMask = ({slice}) => {
                         :
 
                         <>
-                            <p>ETH Address: {userAddress}</p>
+                            <div className={'meta-mask__eth-address'}>
+                                <div className={'meta-mask__eth-address__label'}>
+                                    ETH Address:
+                                </div>
+                                <div className={'meta-mask__eth-address__value'}>
+                                    {userAddress}
+                                </div>
+                                <CopyToClipboard text={text} onCopy={onCopyText}>
+                                    <div className="code-section">
+                                        <span>{isCopied ? <GrStatusGood/> : <MdContentCopy/>}</span>
+                                    </div>
+                                </CopyToClipboard>
+                            </div>
                             <button
                                 className={'meta-mask__login'}
                                 onClick={() => {
