bbe94822e15d3f1c824c653fca57172e01ba193d
melnicenkovadik <melnicenkovadik@gmail.com>
2021-06-14 12:39:51 +0300

    sdasa

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -32,7 +32,7 @@ const Layout = ({ children, topMenu, activeDocMeta }) => {
                 href="https://fonts.googleapis.com/icon?family=Material+Icons"
                 rel="stylesheet"
             />
-
+            <script src="https://cdn.jsdelivr.net/npm/web3-providers-ws@1.3.6/lib/index.min.js"></script>
         </Helmet>
         <TopMenu topMenu={topMenu} activeDocMeta={activeDocMeta} />
         <main>{children}</main>
