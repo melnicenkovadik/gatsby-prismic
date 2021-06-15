9d2819705cb3d4d5de96bb5bfdb533a018134835
melnicenkovadik <melnicenkovadik@gmail.com>
2021-06-15 17:27:04 +0300

    sda

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,6 @@
 import * as React from 'react'
 import { PreviewStoreProvider } from 'gatsby-source-prismic'
-const loadableReady = require("@loadable/component").loadableReady
 
-exports.replaceHydrateFunction = () => {
-    return (element, container, callback) => {
-        loadableReady(() => {
-            ReactDOM.render(element, container, callback)
-        })
-    }
-}
 export const wrapRootElement = ({ element }) => (
   <PreviewStoreProvider initialEnabled={true}>{element}</PreviewStoreProvider>
 )
